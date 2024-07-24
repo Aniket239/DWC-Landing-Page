aaa5aa328607bb5daff3557b5ca28be1f1fdd68d
aniket239 <aniketkrbiswas239@gmail.com>
2024-07-24 10:59:45 +0530

    Floor Plans mobile

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
 
 // ===================================== Add shadow in nav ================================
 const nav = document.querySelector('nav');
-
-// Function to add shadow when scrolled
 function addShadowOnScroll() {
-    // Get the current scroll position
     let scrollPosition = window.scrollY;
-
-    // Check if the scroll position is greater than 0 (page is scrolled)
     if (scrollPosition > 0) {
-        // Add the shadow class to the nav element
         nav.style.boxShadow = "0 4px 15px rgba(0, 0, 0, 0.3)";
     } else {
-        // Remove the shadow class if scroll position is 0
         nav.style.boxShadow = "none";
     }
 }
 window.addEventListener('scroll', addShadowOnScroll);
-
 // ================================================== scroll to section =========================================================
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('nav ul li a');
-
     navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
+        link.addEventListener('click', function (e) {
             e.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
-            const offset = 70; // Adjust this value based on your fixed navbar height
-
+            const offset = 70;
             window.scrollTo({
                 top: targetSection.offsetTop - offset,
                 behavior: 'smooth'
@@ -39,32 +29,30 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function openMenu(){
+function openMenu() {
     var checkbox = document.getElementById('hamburger-checkbox');
     const mobileNav = document.querySelector('.mobile-nav')
-    // Now, you can check if the checkbox is checked or not
     if (checkbox.checked) {
-      console.log('The checkbox is checked');
-      mobileNav.style.display = 'flex';
+        console.log('The checkbox is checked');
+        mobileNav.style.display = 'flex';
     } else {
-      console.log('The checkbox is not checked');
-      mobileNav.style.display = 'none';
-    }    
+        console.log('The checkbox is not checked');
+        mobileNav.style.display = 'none';
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
     const mobileNav = document.querySelector('.mobile-nav')
     var checkbox = document.getElementById('hamburger-checkbox');
     mobileNavLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
+        link.addEventListener('click', function (e) {
             mobileNav.style.display = 'none';
             checkbox.checked = false;
             e.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
-            const offset = 70; // Adjust this value based on your fixed navbar height
-
+            const offset = 70; 
             window.scrollTo({
                 top: targetSection.offsetTop - offset,
                 behavior: 'smooth'
@@ -73,19 +61,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-
-
 // ================================================== nav items animation =========================================================
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('#nav-links li');
 
     navLinks.forEach((link, index) => {
         setTimeout(() => {
             link.classList.add('animate');
-        }, 200 * index); // Adjust the delay as needed
+        }, 200 * index);
     });
 });
 
@@ -93,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 const aboutProjectSection = document.querySelector('#about-the-project');
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -102,28 +85,24 @@ const observer = new IntersectionObserver(entries => {
         }
     });
 });
-
 observer.observe(aboutProjectSection);
 
 // ============================================== form ============================================
 function enquiryForm() {
     const formContainer = document.getElementById('form-container');
     const body = document.getElementsByTagName('body')[0];
-    formContainer.style.display = 'grid'; // Display the form container
+    formContainer.style.display = 'grid'; 
     body.style.overflow = 'hidden';
 }
-
-// Add an event listener to the "Download Brochure" button
 const downloadBrochureBtn = document.querySelector('.download-brochure');
 downloadBrochureBtn.addEventListener('click', enquiryForm);
 
 const closeButton = document.getElementById('close-button');
 closeButton.addEventListener('click', closeForm);
-
-function closeForm(){
+function closeForm() {
     const formContainer = document.getElementById('form-container');
     const body = document.getElementsByTagName('body')[0];
-    formContainer.style.display = 'none'; // Display the form container
+    formContainer.style.display = 'none'; 
     body.style.overflow = 'auto';
 }
 
@@ -142,7 +121,7 @@ function clearValidityMessage(element) {
     }
 }
 
-function validatePhone(element){
+function validatePhone(element) {
     console.log(element.value);
     if (element.value !== '') {
         element.classList.add('has-value');
@@ -151,12 +130,10 @@ function validatePhone(element){
     }
 }
 
-
-
-document.getElementById('phoneInput').addEventListener('input', function() {
+document.getElementById('phoneInput').addEventListener('input', function () {
     let phoneValue = this.value;
     let regex = /^\d{10}$/;
-    
+
     if (!regex.test(phoneValue)) {
         this.setCustomValidity('Please enter a valid 10-digit mobile number');
     } else {
@@ -179,19 +156,16 @@ function showTab(tabIndex) {
         tabContents[tabIndex - 1].classList.remove('hidden');
         tabContents[tabIndex - 1].classList.add('active');
     }, 300);
-    
-    // Manage tab focus
+
     for (let tab of tabs) {
         tab.classList.remove('focus');
     }
     tabs[tabIndex - 1].classList.add('focus');
 }
 
-// Initialize the first tab as active and focused
 tabs[0].classList.add('focus');
 showTab(1);
 
-// Modal functionality
 const modal = document.getElementById('modal');
 const modalImage = document.getElementById('modal-image');
 
@@ -204,10 +178,9 @@ function closeModal() {
     modal.style.display = "none";
 }
 
-// Attach click event listeners to img-container elements
 const imgContainers = document.getElementsByClassName('img-container');
 for (let container of imgContainers) {
-    container.addEventListener('click', function() {
+    container.addEventListener('click', function () {
         openModal(this);
     });
 }
@@ -239,8 +212,7 @@ function prevSlide() {
     showSlide(currentSlide - 1);
 }
 
-// Auto slide
-setInterval(nextSlide, 5000); // Change slide every 3 seconds
+setInterval(nextSlide, 5000); 
 
 
 // =========================================== Floor Plan =============================================
@@ -248,28 +220,31 @@ setInterval(nextSlide, 5000); // Change slide every 3 seconds
 // ========== master plan =========================
 let masterPlanCurrent = 0;
 
-function masterfloorPlansShowSlide(index) {
-    const masterSlides = document.querySelectorAll('.floor-plans-item');
+function masterfloorPlansShowSlide(masterPlanIndex) {
+    const masterSlides = document.querySelectorAll('.master-floor-plans-item');
     const mastertotalSlides = masterSlides.length;
     console.log(mastertotalSlides);
-    if (index >= mastertotalSlides) {
+    if (masterPlanIndex >= mastertotalSlides) {
         masterPlanCurrent = 0;
-    } else if (index < 0) {
+    } else if (masterPlanIndex < 0) {
         masterPlanCurrent = mastertotalSlides - 1;
     } else {
-        masterPlanCurrent = index;
+        masterPlanCurrent = masterPlanIndex;
     }
-    const offset = -masterPlanCurrent * 100;
-    document.querySelector('.master-floor-plans-container').style.transform = `translateX(${offset}%)`;
+    const masterPlanOffset = -masterPlanCurrent * 100;
+    document.querySelector('.master-floor-plans-container').style.transform = `translateX(${masterPlanOffset}%)`;
 }
 
-function masterFloorPlansprevSlide() {
+function masterFloorPlansNextSlide() {
     masterfloorPlansShowSlide(masterPlanCurrent + 1);
 }
 
 function masterFloorPlansprevSlide() {
     masterfloorPlansShowSlide(masterPlanCurrent - 1);
 }
+
+setInterval(masterFloorPlansNextSlide, 5000); 
+
 // ============== foor plan ========================
 let current = 0;
 
@@ -286,17 +261,16 @@ function floorPlansShowSlide(index) {
     const offset = -current * 100;
     document.querySelector('.floor-plans-container').style.transform = `translateX(${offset}%)`;
 }
-
 function floorPlansnextSlide() {
     floorPlansShowSlide(current + 1);
 }
-
 function floorPlansprevSlide() {
     floorPlansShowSlide(current - 1);
 }
+setInterval(floorPlansnextSlide, 5000); 
 
-// Auto slide
-setInterval(floorPlansnextSlide, 5000); // Change slide every 3 seconds
+
+// ============================= Floor plan tabs ====================================
 
 const floorPlansTabs = document.getElementsByClassName('floor-plans-tab');
 const floorPlansTabContents = document.getElementsByClassName('floor-plans-tab-content');
@@ -311,42 +285,10 @@ function floorPlanShowTab(tabIndex) {
         floorPlansTabContents[tabIndex - 1].classList.remove('hidden');
         floorPlansTabContents[tabIndex - 1].classList.add('active');
     }, 300);
-    
-    // Manage tab focus
     for (let tab of floorPlansTabs) {
         tab.classList.remove('focus');
     }
     floorPlansTabs[tabIndex - 1].classList.add('focus');
 }
-
-// Initialize the first tab as active and focused
 floorPlansTabs[0].classList.add('focus');
 floorPlanShowTab(1);
-
-
-// Modal functionality
-// const modal = document.getElementById('modal');
-// const modalImage = document.getElementById('modal-image');
-
-// function openModal(imageElement) {
-//     modal.style.display = "flex";
-//     modalImage.src = imageElement.querySelector('img').src;
-// }
-
-// function closeModal() {
-//     modal.style.display = "none";
-// }
-
-// // Attach click event listeners to img-container elements
-// const imgContainers = document.getElementsByClassName('img-container');
-// for (let container of imgContainers) {
-//     container.addEventListener('click', function() {
-//         openModal(this);
-//     });
-// }
-
-
-
-
-
-
